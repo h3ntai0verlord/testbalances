5860b100cac65e05af3ce327514d183800fb07a5
h3ntai0verlord <95223891+h3ntai0verlord@users.noreply.github.com>
2023-03-17 09:26:51 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const urlParams = new URLSearchParams(window.location.search);
+const apiKey = urlParams.get('api_key');
+const apiSecret = urlParams.get('secret');
+
+fetch('https://api.3commas.io/public/api/v1/accounts', {
+  headers: {
+    'APIKEY': apiKey,
+    'SECRET': apiSecret,
+  }
+})
+.then(response => response.json())
+.then(accounts => {
+  const balances = accounts.map(account => {
+    return `<li>${account.currency}: ${account.available}</li>`;
+  }).join('');
+
+  document.querySelector('#balances').innerHTML = balances;
+})
+.catch(error => {
+  console.error('Error fetching balances:', error);
+});
